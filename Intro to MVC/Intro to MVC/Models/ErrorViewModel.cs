a716cee594f41a82a226c6c0f8540a35b3982ace
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-09-20 14:12:05 +0300

    Add files via upload

---
+++ Intro to MVC/Intro to MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intro_to_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
