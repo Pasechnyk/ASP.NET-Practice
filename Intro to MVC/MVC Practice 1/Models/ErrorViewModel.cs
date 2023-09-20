f9886b7bdcf0dbaef1655b0f03df9156566880e3
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-09-20 14:08:30 +0300

    Delete Intro to MVC/MVC Practice 1 directory

--- Intro to MVC/MVC Practice 1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_Practice_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
